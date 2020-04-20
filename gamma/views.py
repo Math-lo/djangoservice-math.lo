12b9c7d1bc3bf9137497d07dc20fe485c27e4132
Kvothes <bearexe51@gmail.com>
2020-04-20 11:38:18 -0500

    Modificación en el retorno de scripts a json

--- gamma/views.py
+++ gamma/views.py
@@ -1,5 +1,5 @@
 from django.template import loader
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
 from . import dbprocess
 
 # Create your views here.
@@ -11,15 +11,16 @@ def index(request):
 def generalGroupQuestionnaire(request,id_group,id_cue):
 	valueTopic = [0,0]
 	nameCuest = ""
+	valueInfo = []
 	try:
 		valueTopic = dbprocess.getGradeGroupQuestionnaire(id_cue, id_group)
 		nameCuest = dbprocess.getNameCuest(id_cue)
 	except Exception as e:
 		print("[X] Error in view generalGroupQuestionnaire: "+str(e))
-	script = "google.charts.load('current', {'packages': ['corechart']});google.charts.setOnLoadCallback(drawChart);"
-	script += "function drawChart(){var data = google.visualization.arrayToDataTable(["
+	script = "google.charts.load('current', {'packages': ['corechart']});google.charts.setOnLoadCallback(drawChart);function drawChart(){var data = google.visualization.arrayToDataTable(["
 	script += "['Campo', 'Calificaciones'],"
 	script += "['Reprobados', {}],".format(valueTopic[1])
+	valueInfo += [['Campo', 'Calificaciones'], ['Reprobados', valueTopic[1]], ['Aprobados', valueTopic[0]]]
 	script += "['Aprobados', {}]".format(valueTopic[0])
 	script += "]);var options = {"
 	script += "title: '{} Grupo {}',is3D: true,".format(nameCuest,id_group)
@@ -27,7 +28,7 @@ def generalGroupQuestionnaire(request,id_group,id_cue):
 	script += "};var chart = new"
 	script += " google.visualization.PieChart(document.getElementById('Group:{}Cuestionario:{}'));".format(id_group,id_cue)
 	script += "chart.draw(data, options);}"
-	return HttpResponse(script)
+	return JsonResponse(valueInfo, safe=False)
 
 def generalQuestionnaire(request,id_cue):
 	nameCuest = ""
@@ -37,37 +38,38 @@ def generalQuestionnaire(request,id_cue):
 		valueTopic = dbprocess.getGradeQuestionnaire(id_cue)
 	except Exception as e:
 		print("[X] Error en view generalQuestionnaire: "+str(e))
-	script = "google.charts.load('current', {'packages': ['corechart']});google.charts.setOnLoadCallback(drawChart);"
-	script += "function drawChart(){var data = google.visualization.arrayToDataTable(["
+	script = "google.charts.load('current', {'packages': ['corechart']});google.charts.setOnLoadCallback(drawChart);function drawChart(){var data = google.visualization.arrayToDataTable(["
 	script += "['Campo', 'Calificaciones'],"
 	script += "['Reprobados', {} ],".format(valueTopic[1])
 	script += "['Aprobados', {}]".format(valueTopic[0])
+	valueInfo = [['Campo', 'Calificaciones'], ['Reprobados', valueTopic[1]], ['Aprobados', valueTopic[0]]]
 	script += "]);var options = {"
 	script += "title: '{}',is3D: true,".format(nameCuest)
 	script += "slices: {0: { color: '#5c6bc0' },1: { color: '#ff9800' }}"
 	script += "};var chart = new"
 	script += " google.visualization.PieChart(document.getElementById('Cuestionario:{}'));".format(id_cue)
 	script += "chart.draw(data, options);}"
-	return HttpResponse(script)
+	return JsonResponse(valueInfo, safe=False)
 
 def generalGrades(request):
 	valueTopic = [0,0]
+	valueInfo = []
 	try:
 		valueTopic = dbprocess.getGeneralGrades()
 	except Exception as e:
 		print("[X] Error en view generalGrades: "+str(e))
-	script = "google.charts.load('current', {'packages': ['corechart']});google.charts.setOnLoadCallback(drawChart);"
-	script += "function drawChart(){var data = google.visualization.arrayToDataTable(["
+	script = "google.charts.load('current', {'packages': ['corechart']});google.charts.setOnLoadCallback(drawChart);function drawChart(){var data = google.visualization.arrayToDataTable(["
 	script += "['Campo', 'Calificaciones'],"
 	script += "['Reprobados', {}],".format(valueTopic[1])
 	script += "['Aprobados', {}]".format(valueTopic[0])
+	valueInfo += [['Campo', 'Calificaciones'], ['Reprobados', valueTopic[1]], ['Aprobados', valueTopic[0]]]
 	script += "]);var options = {"
 	script += "title: 'Numero de Aprobados y Reprobados de Todos los Alumnos',is3D: true,"
 	script += "slices: {0: { color: '#5c6bc0' },1: { color: '#ff9800' }}"
 	script += "};var chart = new"
 	script += " google.visualization.PieChart(document.getElementById('generalGrades'));"
 	script += "chart.draw(data, options);}"
-	return HttpResponse(script)
+	return JsonResponse(valueInfo, safe=False)
 
 def generalColumnGroupTopic(request,id_group):
 	temas = {}
@@ -97,18 +99,21 @@ def generalColumnGroupTopic(request,id_group):
 				temas[valin] = array
 	except Exception as e:
 		print("[X] Error en view generalColumnGroupTopic: "+str(e))
+	valueInfo = []
 	script = "google.charts.load('current', {'packages':['bar']}); google.charts.setOnLoadCallback(drawChart);"
 	script += "function drawChart() { var data = google.visualization.arrayToDataTable(["
 	script += "['Tema', 'Correctos', 'Incorrectos'],"
+	valueInfo += [['Tema', 'Correctos', 'Incorrectos']]
 	for keys in temas:
 		script += "['{}',{},{}],".format(dbprocess.getNameTopic(keys),str(temas[keys][0]),str(temas[keys][1]))
+		valueInfo += [[dbprocess.getNameTopic(keys),str(temas[keys][0]),str(temas[keys][1])]]
 	script += "]);"
 	script += "var options = { chart: {"
 	script += "title: 'Temas del Grupo {}'".format(id_group)
 	script += ",subtitle: 'Aciertos mas Correctos y Incorrectos en Temas',}, colors: ['#ff9800','#5c6bc0'] };"
 	script += "var chart = new google.charts.Bar(document.getElementById('generalTopicGroup: {}'));".format(id_group)
 	script += "chart.draw(data, google.charts.Bar.convertOptions(options));}"
-	return HttpResponse(script)
+	return JsonResponse(valueInfo, safe=False)
 
 def generalColumnTopic(request):
 	temas = {}
@@ -141,15 +146,17 @@ def generalColumnTopic(request):
 	script = "google.charts.load('current', {'packages':['bar']}); google.charts.setOnLoadCallback(drawChart);"
 	script += "function drawChart() { var data = google.visualization.arrayToDataTable(["
 	script += "['Tema', 'Correctos', 'Incorrectos'],"
+	valueInfo = [['Tema', 'Correctos', 'Incorrectos']]
 	for keys in temas:
 		script += "['{}',{},{}],".format(dbprocess.getNameTopic(keys),str(temas[keys][0]),str(temas[keys][1]))
+		valueInfo += [[dbprocess.getNameTopic(keys),temas[keys][0],temas[keys][1]]]
 	script += "]);"
 	script += "var options = { chart: {"
 	script += "title: 'Temas'"
 	script += ",subtitle: 'Aciertos mas Correctos y Incorrectos en Temas',}, colors: ['#ff9800','#5c6bc0']};"
 	script += "var chart = new google.charts.Bar(document.getElementById('generalTopic'));"
 	script += "chart.draw(data, google.charts.Bar.convertOptions(options));}"
-	return HttpResponse(script)
+	return JsonResponse(valueInfo, safe=False)
 
 def questionnaireGradeGroup(request,id_group,id_cue):
 	temas = {}
@@ -184,15 +191,17 @@ def questionnaireGradeGroup(request,id_group,id_cue):
 	script = "google.charts.load('current', {'packages':['bar']}); google.charts.setOnLoadCallback(drawChart);"
 	script += "function drawChart() { var data = google.visualization.arrayToDataTable(["
 	script += "['Preguntas', 'Correctos', 'Incorrectos'],"
+	valueInfo = [['Preguntas', 'Correctos', 'Incorrectos']]
 	for keys in temas:
 		script += "['{}',{},{}],".format(dbprocess.getNameQuestion(keys),str(temas[keys][0]),str(temas[keys][1]))
+		valueInfo += [[dbprocess.getNameQuestion(keys), temas[keys][0], temas[keys][1]]]
 	script += "]);"
 	script += "var options = { chart: {"
 	script += "title: 'Aciertos del Grupo {} del {}'".format(id_group,nameCuest)
 	script += ",subtitle: 'Aciertos mas Correctos y Incorrectos en Preguntas',}, colors: ['#ff9800','#5c6bc0']};"
 	script += "var chart = new google.charts.Bar(document.getElementById('questions-Group:{} Cue:{}'));".format(id_group,id_cue)
 	script += "chart.draw(data, google.charts.Bar.convertOptions(options));}"
-	return HttpResponse(script)
+	return JsonResponse(valueInfo, safe=False)
 
 def questionnaireGrades(request,id_cue):
 	temas = {}
@@ -227,44 +236,40 @@ def questionnaireGrades(request,id_cue):
 	script = "google.charts.load('current', {'packages':['bar']}); google.charts.setOnLoadCallback(drawChart);"
 	script += "function drawChart() { var data = google.visualization.arrayToDataTable(["
 	script += "['Preguntas', 'Correctos', 'Incorrectos'],"
+	valueInfo = [['Preguntas', 'Correctos', 'Incorrectos']]
 	for keys in temas:
 		script += "['{}',{},{}],".format(dbprocess.getNameQuestion(keys),str(temas[keys][0]),str(temas[keys][1]))
+		valueInfo += [[dbprocess.getNameQuestion(keys), temas[keys][0], temas[keys][1]]]
 	script += "]);"
 	script += "var options = { chart: {"
 	script += "title: 'Aciertos del {}'".format(nameCuest)
 	script += ",subtitle: 'Aciertos mas Correctos y Incorrectos en Preguntas',}, colors: ['#ff9800','#5c6bc0']};"
 	script += "var chart = new google.charts.Bar(document.getElementById('generalQuestionsCue:{}'));".format(id_cue)
 	script += "chart.draw(data, google.charts.Bar.convertOptions(options));}"
-	return HttpResponse(script)
+	return JsonResponse(valueInfo, safe=False)
 
 ##POR ALUMNO###########################
 
 def questionnaireAlumno(request,id_alumno,id_cue):
 	nameCuest = ""
-	answers = [[0], [0]]
+	answers = [[],[]]
 	try:
 		nameCuest = dbprocess.getNameCuest(id_cue)
 		answers = dbprocess.getGradeQuestionnaireAlumno(id_alumno, id_cue)
 	except Exception as e:
-		nameCuest = ""
-		answers = [[0], [0]]
 		print("[X] Error en view questionnaireAlumno: "+str(e))
-	script = "google.charts.load('current', {'packages': ['corechart']});google.charts.setOnLoadCallback(drawChart);"
-	script += "function drawChart(){var data = google.visualization.arrayToDataTable(["
+	script = "google.charts.load('current', {'packages': ['corechart']});google.charts.setOnLoadCallback(drawChart);function drawChart(){var data = google.visualization.arrayToDataTable(["
 	script += "['Campo', 'Calificacion Cuestionario'],"
-	if len(answers) > 0:
-		script += "['Correctas', {}],".format(len(answers[0]))
-		script += "['Incorrectas', {}]".format(len(answers[1]))
-	else:
-		script += "['Correctas', {}],".format(str(0))
-		script += "['Incorrectas', {}]".format(str(0))
+	script += "['Correctas', {}],".format(len(answers[0]))
+	script += "['Incorrectas', {}]".format(len(answers[1]))
+	valueInfo = [['Campo', 'Calificación cuestionario'], ['Correctas', len(answers[0])], ['Incorrectas', len(answers[1])]]
 	script += "]);var options = {"
 	script += "title: '{}',is3D: true,".format(nameCuest)
 	script += "slices: {1: { color: '#5c6bc0' },0: { color: '#ff9800' }}"
 	script += "};var chart = new"
 	script += " google.visualization.PieChart(document.getElementById('questionnarie:{}alu:{}'));".format(id_cue,id_alumno)
 	script += "chart.draw(data, options);}"
-	return HttpResponse(script)
+	return JsonResponse(valueInfo, safe=False)
 
 def questionnairesAlumnoTopic(request,id_alumno,id_tem):
 	nameTopic = ""
@@ -281,15 +286,15 @@ def questionnairesAlumnoTopic(request,id_alumno,id_tem):
 				incorrectas.append(ans)
 	except Exception as e:
 		print("[X] Error en view questionnairesAlumnoTopic: "+str(e))
-	script = "google.charts.load('current', {'packages': ['corechart']});google.charts.setOnLoadCallback(drawChart);"
-	script += "function drawChart(){var data = google.visualization.arrayToDataTable(["
+	script = "google.charts.load('current', {'packages': ['corechart']});google.charts.setOnLoadCallback(drawChart);function drawChart(){var data = google.visualization.arrayToDataTable(["
 	script += "['Campo', 'Calificacion Cuestionario'],"
 	script += "['Correctas', {}],".format(len(correctas))
 	script += "['Incorrectas', {}]".format(len(incorrectas))
+	valueInfo = [['Campo', 'Calificación cuestionario'], ['Correctas', len(correctas)], ['Incorrectas', len(incorrectas)]]
 	script += "]);var options = {"
 	script += "title: '{}',".format(nameTopic)
 	script += "is3D: true,slices: {1: { color: '#5c6bc0' },0: { color: '#ff9800' }}"
 	script += "};var chart = new"
 	script += " google.visualization.PieChart(document.getElementById('topic:{}alu:{}'));".format(id_tem,id_alumno)
 	script += "chart.draw(data, options);}"
-	return HttpResponse(script)
+	return JsonResponse(valueInfo, safe=False)
